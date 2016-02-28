7a9a051137d908914323ecf4eb5013c0cb0e2a41
baotang <tlyspa@qq.com>
2016-02-28 14:09:00 +0800

    first-commit

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+// Generated on 2016-02-26 using generator-angular-fullstack 3.3.0
+'use strict';
+
+module.exports = function (grunt) {
+  var localConfig;
+  try {
+    localConfig = require('./server/config/local.env');
+  } catch(e) {
+    localConfig = {};
+  }
+
+  // Load grunt tasks automatically, when needed
+  require('jit-grunt')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+  });
+};
